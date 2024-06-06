b3ce8d25af1329434cf6c6f04167f7d9d84167c8
Rukuia <rukuiakaarbekkyzy@gmail.com>
2024-06-06 14:51:29 +0600

    clean unused imports in Book

--- src/main/groovy/library/models/Booked.java
+++ src/main/groovy/library/models/Booked.java
@@ -3,10 +3,12 @@ package library.models;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
 import java.time.LocalDate;
 
 @Entity
-@Getter @Setter
+@Getter
+@Setter
 public class Booked {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
