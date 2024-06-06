7b2b49652d0b436a48f6540356077d42f08ad353
Rukuia <rukuiakaarbekkyzy@gmail.com>
2024-06-06 14:47:56 +0600

    clean code

--- src/main/groovy/library/models/Booked.java
+++ src/main/groovy/library/models/Booked.java
@@ -11,6 +11,7 @@ public class Booked {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @ManyToOne
     @JoinColumn(name = "book_id")
     private Book book;
@@ -18,6 +19,7 @@ public class Booked {
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
+
     private LocalDate takeDate;
 
 }
